12a8a007c51d7f709c008eba9fa97b63986546f3
kleber.toyota <kleber.toyota@concretesolutions.com.br>
2015-09-29 15:51:36 -0300

    Added pt support and modified to follow the user language to render emergency buttons

--- app/src/main/java/com/kat/fivethings/DetailActivity.java
+++ app/src/main/java/com/kat/fivethings/DetailActivity.java
@@ -28,9 +28,9 @@ public class DetailActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/kat/fivethings/MainActivity.java
+++ app/src/main/java/com/kat/fivethings/MainActivity.java
@@ -8,7 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,30 +20,117 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button emergencyNumberOneButton = (Button) findViewById(R.id.emergencyNumberOne);
-        emergencyNumberOneButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:" + getString(R.string.emergencyNumberOne)));
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
-            }
-        });
+        // the layout on which you are working
+        LinearLayout emergencyNumberLinearLayout = (LinearLayout) findViewById(R.id.emergencyNumberLinearLayout);
+        // device language to show emergency number button
+        String deviceLanguage = Locale.getDefault().getLanguage();
+        if (deviceLanguage.equalsIgnoreCase("en")) { // us has one emergency number
+            // create button and set the properties for button
+            Button emergencyNumber911Button = new Button(this);
+            emergencyNumber911Button.setText(super.getString(R.string.emergencyNumber911));
+            LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
+            emergencyNumber911Button.setLayoutParams(linearLayoutParams);
 
-        final Button emergencyNumberTwoButton = (Button) findViewById(R.id.emergencyNumberTwo);
-        emergencyNumberTwoButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:" + getString(R.string.emergencyNumberTwo)));
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
+            // starts a phone call intent
+            emergencyNumber911Button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:" + getString(R.string.emergencyNumber911)));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
                 }
-            }
-        });
+            });
+            //add button to the layout
+            emergencyNumberLinearLayout.addView(emergencyNumber911Button);
+        } else if(deviceLanguage.equalsIgnoreCase("pt")) { // brazil has two emergency numbers
+            LinearLayout.LayoutParams lineLayoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
 
+
+            // create button and set the properties for button
+            Button emergencyNumberOneButton = new Button(this);
+            emergencyNumberOneButton.setText(getString(R.string.emergencyNumberOne));
+            emergencyNumberOneButton.setLayoutParams(lineLayoutParams);
+
+            // starts a phone call intent
+            emergencyNumberOneButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:" + getString(R.string.emergencyNumberOne)));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+                }
+            });
+            //add button to the layout
+            emergencyNumberLinearLayout.addView(emergencyNumberOneButton);
+
+            // create button and set the properties for button
+            Button emergencyNumberTwoButton = new Button(this);
+            emergencyNumberTwoButton.setText(getString(R.string.emergencyNumberTwo));
+            emergencyNumberTwoButton.setLayoutParams(lineLayoutParams);
+
+            // starts a phone call intent
+            emergencyNumberTwoButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:" + getString(R.string.emergencyNumberTwo)));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+                }
+            });
+            //add button to the layout
+            emergencyNumberLinearLayout.addView(emergencyNumberTwoButton);
+        }
+
+        // creates a click event and starts detail activity for CPR
         TextView cprTextView = (TextView) findViewById(R.id.cprTextView);
         cprTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // creates a click event and starts detail activity for heimlich maneuver
+        TextView heimlichManeuverTextView = (TextView) findViewById(R.id.heimlichManeuverTextView);
+        heimlichManeuverTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // creates a click event and starts detail activity for bleeding hemorrhage
+        TextView bleedingHemorrhageTextView = (TextView) findViewById(R.id.bleedingHemorrhageTextView);
+        bleedingHemorrhageTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // creates a click event and starts detail activity for drowning
+        TextView drowningTextView = (TextView) findViewById(R.id.drowningTextView);
+        drowningTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // creates a click event and starts detail activity for burns
+        TextView burnsTextView = (TextView) findViewById(R.id.burnsTextView);
+        burnsTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, DetailActivity.class);
@@ -64,9 +154,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
